cea0a6ee694c3941beac80cc096c347a8d977206
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-31 00:36:48 +0200

    added integrate method

--- Lab3/code/approximation.py
+++ Lab3/code/approximation.py
@@ -15,3 +15,22 @@ def create_matrix_and_symbols():
     ]
     symbols = [c1, c2, c3, c4, m1, m2, m3]
     return matrix_a, symbols
+
+
+def to_vector(matrix, row):
+    if 0 <= row < len(matrix):
+        return matrix[row]
+    else:
+        print(f"Row index {row} is out of bounds.")
+        return None
+
+
+def integrate_sq_deviation(y_matrix, y_approximation):
+    delta_t = 0.2
+    n = y_matrix.shape[1]
+    integral_sum = 0
+    for i in range(n):
+        vector_difference = to_vector(y_matrix, i) - to_vector(y_approximation, i)
+        partial = np.dot(vector_difference, vector_difference)
+        integral_sum += partial
+    return integral_sum * delta_t
